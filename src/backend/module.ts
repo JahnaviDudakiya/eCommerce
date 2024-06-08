44fd050984359373920240f6ccb1b36df731626c
JahnaviDudakiya <jahnavi.dudakiya.213@gmail.com>
2024-06-08 22:55:52 +0530

    Delete src directory

--- src/backend/module.ts
+++
@@ -1,7 +0,0 @@
-import { OrderModule } from './order/order.module';
-import { ProductModule } from './product/product.module';
-import { UserModule } from './user/user.module';
-
-const BackendModules = [UserModule, ProductModule, OrderModule];
-
-export { BackendModules };
